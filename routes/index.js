18d3264621be11f0167909823fca1dcb7d94f2ab
miche1e <michele.na7ali@gmail.com>
2020-12-09 12:00:56 +0100

    adding authentication and login errors handling

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const admin = require("firebase-admin");
+const firebase = require("firebase");
 const serviceAccount = require("../serviceAccount.json");
 
 const router = express.Router();
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAUnTB7bzy0ZQNJplLk3Jcl_4NGqk75X-A",
+    authDomain: "digital-quotes-fd365.firebaseapp.com",
+    projectId: "digital-quotes-fd365",
+    storageBucket: "digital-quotes-fd365.appspot.com",
+    messagingSenderId: "799639785474",
+    appId: "1:799639785474:web:e86fd220ba1c36b2ac971b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 quotes = [];
+logged = false;
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -22,6 +36,22 @@ async function updateQuotes() {
     list.forEach(doc => quotes.push(doc.data()));
 }
 
+router.post("/login", async (req, res) => {
+    try{
+        if(!req.body.email){
+            return res.status(400).send({error: "Bad request, missing mail field!"});
+        }else if(!req.body.password){
+            return res.status(400).send({error: "Bad request, missing the password field!"});
+        }
+        const user = await firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password);
+        const token = await user.user.getIdToken();
+        logged = true;
+        return res.status(201).json({ token });
+    }catch(error){
+        return res.status(500).send({error: error.toString()});
+    }
+});
+
 router.get("/quotes", async (req, res) => {
     try {
         await updateQuotes();
@@ -35,7 +65,7 @@ router.get("/quotes/:id", async (req, res) => {
     try {
         const quote = await db.collection('quotes').doc(req.params.id).get();
         if (!quote.data()) {
-            return res.status(404).json({message: "user not found"});
+            return res.status(404).json({message: "Quote not found"});
         }
         return res.status(200).json(quote.data());
     } catch (error) {
@@ -45,9 +75,12 @@ router.get("/quotes/:id", async (req, res) => {
 
 router.post("/quotes", async (req, res) => {
     try {
+        if(!logged){
+            return res.status(400).send({error: "Unauthorized, you must log in for this action!"});
+        }
         await updateQuotes();
         if (!req.body.author || !req.body.quote) {
-            return res.status(400).json({message: "Bad request: missing quote or author in the request body!"});
+            return res.status(400).send({error: "Unauthorized, you must log in for this action!"});
         }
         const newId = genId();
         let newQuote = {
@@ -64,6 +97,9 @@ router.post("/quotes", async (req, res) => {
 
 router.patch("/quotes/:id", async (req, res) => {
     try {
+        if(!logged){
+            return res.status(400).send({error: "Unauthorized, you must log in for this action!"});
+        }
         quotes.length = 0;
         const list = await db.collection("quotes").get();
         list.forEach(doc => quotes.push(doc.data()));
@@ -99,6 +135,9 @@ router.patch("/quotes/:id", async (req, res) => {
 
 router.delete("/quotes/:id", async (req, res) => {
     try {
+        if(!logged){
+            return res.status(400).send({error: "Unauthorized, you must log in for this action!"});
+        }
 
         const q = await db.collection('quotes').doc(req.params.id).get();
         if (!q.data()) {
